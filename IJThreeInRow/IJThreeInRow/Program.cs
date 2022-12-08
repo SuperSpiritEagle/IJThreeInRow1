c281b2145e335520a42af5412de8d9d79afb30fe
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-08 14:17:45 +0500

    Init

---
+++ IJThreeInRow/IJThreeInRow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IJThreeInRow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int filledRows = 17;
+            int seriesPictures = 3;
+            int imagesAlbum = 0;
+            int notIncludedInAlbum = 2;
+
+            for (int i = 0; i < filledRows; i++)
+            {
+                for (int j = 0; j < seriesPictures; j++)
+                {
+                    Console.Write((imagesAlbum++) + "\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Полностью заполненных рядов можно будет вывести {filledRows}" +
+                $"\nкартинок сверх меры {notIncludedInAlbum}");
+
+        }
+    }
+}
