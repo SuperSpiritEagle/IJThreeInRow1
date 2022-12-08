3ee76d3bb737376d14ce84837d82c6f3dea66177
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-08 15:24:35 +0500

    IJThreeInRow1

---
+++ IJThreeInRow01/IJThreeInRow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IJThreeInRow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int filledRows = 52;
+            int seriesPictures = 3;
+            int imagesAlbum ;
+            int notIncludedInAlbum;
+            imagesAlbum = filledRows / seriesPictures;
+            imagesAlbum = imagesAlbum * seriesPictures;
+            notIncludedInAlbum = filledRows - imagesAlbum;
+            Console.WriteLine($"Полностью заполненных рядов можно будет вывести {imagesAlbum}" +
+                $"\nкартинок сверх меры {notIncludedInAlbum}");
+        }
+    }
+}
